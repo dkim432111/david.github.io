24b0c77097e2939b96c94375ff21f27b2f1b1a75
dkim432111 <z5361471@ad.unsw.edu.au>
2025-08-07 13:19:20 +1000

    Add files via upload

---
+++ z5361471_MDIA2010_FINAL/javainyeon.js
@@ -0,0 +1,8 @@
+java.js
+
+//Popup function from w4
+
+function mypopup() {
+    mywindow = window.open("popup.html", "mywindow", "location=1,status=1,scrollbars=1,  width=600,height=700");
+    mywindow.moveTo(0, 0);
+}
